1a006f44a0e3631ad32aaa80dab76deae8ca5efa
Chaqs <41303233+Chaqs@users.noreply.github.com>
2020-02-11 23:15:15 +0000

    Add files via upload

---
+++ src/Chaqs/AdvancedHeal/Main.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+namespace Chaqs\AdvancedHeal;
+
+
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\plugin\Plugin;
+
+
+
+
+class Main extends PluginBase implements Listener{
+    
+
+    public function onEnable(): void{
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        
+
+    }
+
+
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        switch(strtolower($label)){
+            case "heal":
+                if(!$sender instanceof Player) return false;
+                if($sender->hasPermission("command.advancedheal")){
+                    $beforehealth = $sender->getHealth();
+                    $maxhealth = $sender->getMaxHealth();
+                    $sender->setHealth(20);
+                    $currenthealth = $sender->getHealth();
+                    $colorcode = "6";
+                    $resetcolor = "r";
+                    $sender->sendMessage("§$colorcode You Have Been Healed! §$resetcolor \nResults: \n §cHealth-Changes:§e " . $beforehealth . "§7/§e" . $maxhealth . "§7 ->§e " . $currenthealth . "§7/§e" . $maxhealth);
+                    $beforefood = $sender->getFood();
+                    $maxfood = $sender->getMaxFood();
+                    $sender->setFood(20);
+                    $currentfood = $sender->getFood();
+                    $sender->sendMessage(" §cFood-Changes:§e " . $beforefood . "§7/§e" . $maxfood . "§7 ->§e " . $currentfood . "§7/§e" . $maxfood);
+                    
+                }else{
+                    $sender->sendMessage("Invalid Permissions");
+                    
+                }
+
+                break;
+
+        }
+
+        return true;
+
+    }
+
+}
